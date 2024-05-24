ccaba761b97e6d3c664553ba8ddcf7f98c0e0fbe
AlexandraGodoy <alexandramichelgodoyhernandez@inued.net>
2024-05-24 08:47:57 -0600

    Add files via upload
    Signed-off-by: AlexandraGodoy <alexandramichelgodoyhernandez@inued.net>

---
+++ SistemaMtricula/MaticulaINued/MaticulaINued/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCrear = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnCrear
+        '
+        Me.btnCrear.Location = New System.Drawing.Point(290, 12)
+        Me.btnCrear.Name = "btnCrear"
+        Me.btnCrear.Size = New System.Drawing.Size(178, 81)
+        Me.btnCrear.TabIndex = 0
+        Me.btnCrear.Text = "Crear conexion"
+        Me.btnCrear.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 124)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(685, 252)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnCrear)
+        Me.Name = "Form1"
+        Me.Text = "Matricula"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCrear As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ SistemaMtricula/MaticulaINued/MaticulaINued/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub btnCrear_Click(sender As Object, e As EventArgs) Handles btnCrear.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source=D:\Programación3\SegundoParcial\SistemaMtricula\basededatos\matriculaINUED(2002-2003).mdb")
+        Try
+
+            con.Open()
+            MsgBox("Conectado")
+
+            Dim query = "Select * from maestro"
+            Dim adap As New OleDbDataAdapter(query, con)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            DataGridView1.DataSource = dt
+        Catch ex As Exception
+            MsgBox("No se conectó por: " & ex.Message)
+        End Try
+    End Sub
+End Class
